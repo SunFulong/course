f03a46425a83e5613f897fd3cf51b3bde559c342
Sun Fulong <sunfulong@nou.com.cn>
2014-03-25 22:41:41 +0800

    Now can listing files.

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,26 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function(course){
+  return function(req, res){
+    url = req.url;
+
+    if (url.charAt(url.length - 1) == '/') {
+      if (url.length > 1) {
+        id = url.substr(1, url.length - 2);
+      } else {
+        id = null;
+      }
+      course.find({parent: id}, {}, function(e, list) {
+        res.render('index', {
+          'list': list
+        });
+      });
+    } else {
+      root = url.split('/')[0]
+      res.render('index', {
+        'url': url
+      });
+    }
+  };
 };
