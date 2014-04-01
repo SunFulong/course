29de0be136720afcc5dafcaad348f86e65fcf6c0
Sun Fulong <sunfulong@nou.com.cn>
2014-04-01 09:01:51 +0800

    Core finished.

--- routes/index.js
+++ routes/index.js
@@ -5,29 +5,32 @@ exports.index = function (course) {
 
     function query(p, name) {
       if (!name) {
-        course.find({parent: p}, {sort: ['type', 'name']}).on('success', function (list) {
-          response.render('index', {'list': list});
+        course.find({parent: p}, {sort: ['type', 'name']}).on('success', function (results) {
+          console.log(results);
+          response.render('list', {list: results});
         });
-      } else {
-        course.findOne({parent: p, name: name}).on('success', function (doc) {
-          if (!doc) {
-            response.status(404);
-            response.send('404 Not Found');
 
-            return;
-          }
-
-          if (!p) {
-            rootUrl = doc.url;
-          }
-          if (!doc.type) {
-            query(doc._id, names.shift());
-          } else {
-            response.redirect(rootUrl + request.url.substr(1));
-          }
-        });
+        return;
       }
-    };
+
+      course.findOne({parent: p, name: name}).on('success', function (result) {
+        if (!result) {
+          response.status(404);
+          response.send('404 Not Found');
+
+          return;
+        }
+
+        if (!p) {
+          rootUrl = result.url;
+        }
+        if (!result.type) {
+          query(result._id, names.shift());
+        } else {
+          response.redirect(rootUrl + request.url.substr(1));
+        }
+      });
+    }
 
     query(null, names.shift());
   };
@@ -35,10 +38,51 @@ exports.index = function (course) {
 
 exports.search = function (course) {
   return function (request, response) {
-    var keyword = request.body.course;
+    var keyword = request.body.course,
+      list, result,
+      name, type, path;
 
-    course.find({name: new RegExp(keyword)}, {sort: ['type', 'name']}).on('success', function (list) {
-      response.render('index', {'list': list});
+    function query(item) {
+      if (!item) {
+        response.render('result', {list: result});
+
+        return;
+      }
+
+      name = item.name;
+      type = item.type;
+      path = [];
+
+      queryPath(item.parent);
+    }
+
+    function queryPath(p) {
+      if (!p) {
+        result.push({ name: name, type: type, path: path.reverse().join('/') });
+
+        query(list.shift());
+        return;
+      }
+
+      course.findOne({_id: p}).on('success', function (result) {
+        path.push(result.name);
+
+        queryPath(result.parent);
+      });
+    }
+
+    keyword = keyword.replace('\\', '\\\\').replace('.', '\\.')
+      .replace('^', '\\^').replace('$', '\\$')
+      .replace('{', '\\{').replace('}', '\\}')
+      .replace('[', '\\[').replace(']', '\\]')
+      .replace('(', '\\(').replace(')', '\\)')
+      .replace('*', '.*').replace('?', '.');
+
+    course.find({name: new RegExp(keyword, 'i')}, {sort: ['type', 'name']}).on('success', function (results) {
+      list = results;
+      result = [];
+
+      query(list.shift());
     });
-  }
-}
+  };
+};
